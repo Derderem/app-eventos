1163ea7817b3f5f60d0b73c32519cb358ca361ad
Derderem <jacobogarver@gmail.com>
2026-05-03 12:15:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -985,9 +985,14 @@ export default function App() {
   adminCitiesList.sort();
 
   var sortedFiltered = filteredEvents.slice().sort(function(a, b) {
+    // 1º Pone los Destacados arriba del todo
     if (a.featured && !b.featured) return -1;
     if (!a.featured && b.featured) return 1;
-    return 0;
+    
+    // 2º Ordena por fecha: el más cercano a HOY va primero
+    var dateA = new Date(a.date).getTime();
+    var dateB = new Date(b.date).getTime();
+    return dateA - dateB;
   });
 
   var featuredEvent = sortedFiltered.length ? sortedFiltered[0] : null;
