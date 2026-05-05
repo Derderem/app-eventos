dd229ea57e446b7e29875fa0d490ddd3144a5c3a
Derderem <jacobogarver@gmail.com>
2026-05-05 20:51:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1510,31 +1510,30 @@ async function confirmSubmitEvent() {
     <button
       </MapContainer>
 
-      <button
-        onClick={() => setMapCenter([40.41, -3.70])}
-        style={{
-          position: 'absolute',
-          bottom: 90,
-          right: 20,
-          zIndex: 1000,
-          background: '#4f46e5',
-          color: 'white',
-          border: 'none',
-          width: 45,
-          height: 45,
-          borderRadius: '50%',
-          boxShadow: '0 10px 25px rgba(0,0,0,.4)',
-          cursor: 'pointer',
-          fontWeight: 900
-        }}
-      >
-        ⌖
-      </button>
+    {/* Botón centrar mapa */}
+    <button
+      onClick={() => setMapCenter([40.41, -3.70])}
+      style={{
+        position: 'absolute',
+        bottom: 90,
+        right: 20,
+        zIndex: 1000,
+        background: '#4f46e5',
+        color: 'white',
+        border: 'none',
+        width: 45,
+        height: 45,
+        borderRadius: '50%',
+        boxShadow: '0 10px 25px rgba(0,0,0,0.4)',
+        cursor: 'pointer',
+        fontWeight: 900
+      }}
+    >
+      ⌂
+    </button>
 
-    </div>
-  )}
-  ⌖
-<MapContainer
+  </div>
+)}
   center={[40.41, -3.70]}
   zoom={6}
   style={{
