7548466a521e267edd38e66161002857c18267ce
Derderem <jacobogarver@gmail.com>
2026-05-06 19:14:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2373,10 +2373,8 @@ async function confirmSubmitEvent() {
               ) : (
                 <button onClick={handleLogout} style={{ background: '#ef4444', color: 'white', fontSize: 10, padding: '8px 15px', borderRadius: 8, border: 'none', fontWeight: 900, cursor: 'pointer' }}>CERRAR SESIÓN</button>
               )}
-            </div>
-</div>
-</div>
-
+    </div>
+  </div>
 )}
 </main>
 
