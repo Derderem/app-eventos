eb9d56ebf72bb187ace3be69952a0be68e42ff36
Derderem <jacobogarver@gmail.com>
2026-05-09 12:03:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function formatDateTime(dateStr) {
     const d = new Date(dateStr);
     return d.toLocaleString('es-ES', {
       day: '2-digit', month: '2-digit', year: 'numeric',
-      hour: '2-digit', minute: '2-digit'S
+      hour: '2-digit', minute: '2-digit'
     });
   } catch {
     return '';
