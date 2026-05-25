12d6719875a4ccbe49687e30bcc173653345b53c
Derderem <jacobogarver@gmail.com>
2026-05-25 18:12:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registrado correctamente'))
+      .catch(err => console.log('Error al registrar Service Worker:', err));
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
