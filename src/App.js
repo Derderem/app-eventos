25fb8c5764f533c9a021ad1e9a848c54109ce9f1
Derderem <jacobogarver@gmail.com>
2026-05-29 18:58:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import ReactCalendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { readShareTarget, clearShareTargetParams } from './utils/shareTarget';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
